4008d4bdbe63d9974ec132c2f4ef331197736f88
soufyanelf <sfoukahi@gmail.com>
2023-09-12 10:17:59 -0400

    add page to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,59 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-import Button from '@mui/material/Button';
+import {
+    Grid
+} from '@mui/material';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+import { promises as fs } from 'fs';
+import path from 'path';
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+import HelpCard from "./help";
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+async function getHelps() {
+    const jsonDirectory = path.join(process.cwd(), 'helpsData');
+    const fileContents = await fs.readFile(jsonDirectory + '/helps', 'utf8');
+    const parsedData = JSON.parse(fileContents.toLocaleString())
+    return parsedData;
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+export default async function HelpsPage() {
+    const helps = await getHelps();
+
+    return (
+        <Grid container>
+            <Grid container style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                marginTop: 50
+            }}>
+                <Image
+                    src="/mosa3ada.svg"
+                    alt="Mosa3ada Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </Grid>
+            <Grid container>
+
+                <Grid style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: 20,
+                    alignItems: "center"
+
+                }}>
+                </Grid>
+            </Grid>
+            <Grid container style={{
+                justifyContent: "center"
+            }}>
+                {helps?.map((help, ind) => (
+                    <Grid xs={10} md={4} key={ind}> 
+                        <HelpCard help={help} />
+                    </Grid>))}
+            </Grid>
+        </Grid>
+    )
 }
