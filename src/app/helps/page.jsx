8391ac2c7bf4b9fb51a7e7d384d0b74c5d228af7
abdellatif-laghjaj <abdelatiflaghjaj@gmail.com>
2023-09-13 00:13:24 +0100

    Update page.jsx

--- src/app/helps/page.jsx
+++ src/app/helps/page.jsx
@@ -6,7 +6,7 @@ import {
 import { promises as fs } from 'fs';
 import path from 'path';
 
-import HelpCard from "./help";
+import HelpCard from "./helps/help";
 import Navbar from '../components/navbar';
 
 async function getHelps() {
