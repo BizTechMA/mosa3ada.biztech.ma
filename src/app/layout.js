71e753bd9d24e7ad6eef97e865d6d867d1d60cd2
Ben Ali <ayoub.inbox@tutanota.com>
2023-09-12 20:47:48 +0100

    deleted unnecessary font setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "مساعدة",
@@ -11,9 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body suppressHydrationWarning className={inter.className}>
-        {children}
-      </body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
