6c8ac643e7127263d938fff52295b47b32cd71db
soufyanelf <sfoukahi@gmail.com>
2023-09-12 10:05:08 -0400

    first version

---
+++ src/app/helps/page.jsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+import {
+    Grid
+} from '@mui/material';
+
+import HelpCard from "./help";
+import { promises as fs } from 'fs';
+import path from 'path';
+
+
+async function getHelps() {
+    const jsonDirectory = path.join(process.cwd(), 'helpsData');
+    const fileContents = await fs.readFile(jsonDirectory + '/helps', 'utf8');
+    const parsedData = JSON.parse(fileContents.toLocaleString())
+    return parsedData;
+}
+
+
+export default async function HelpsPage() {
+    const helps = await getHelps();
+
+    return (
+        <Grid container>
+            <Grid container style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                marginTop: 50
+            }}>
+                <Image
+                    src="/mosa3ada.svg"
+                    alt="Mosa3ada Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </Grid>
+            <Grid container>
+
+                <Grid style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: 20,
+                    alignItems: "center"
+
+                }}>
+                </Grid>
+            </Grid>
+            <Grid container style={{
+                justifyContent: "center"
+            }}>
+                {helps?.map((help, ind) => (
+                    <Grid xs={10} md={4}> 
+                        <HelpCard help={help} key={ind} />
+                    </Grid>))}
+            </Grid>
+        </Grid>
+    )
+}
