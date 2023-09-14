4d990e7cf67568254b709e3dd53844908d9c3f41
suzuya1331 <hasana13work@gmail.com>
2023-09-14 21:33:26 +0100

    fix Error: path is not defined in local env

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,24 @@
-import Image from "next/image";
-import { Container, Grid, Typography } from "@mui/material";
-
-import HelpCard from "./helps/help";
-import Header from "@/components/Header";
-import getAllDocuments from "@/utils/firebase/firestore/getAllDocuments";
+import Image from 'next/image';
+import { Container, Grid, Typography } from '@mui/material';
+import path from 'path';
+import HelpCard from './helps/help';
+import Header from '@/components/Header';
+import getAllDocuments from '@/utils/firebase/firestore/getAllDocuments';
+import fs from 'fs/promises';
 
 async function getHelps() {
   let data = [];
   if (
-    process.env.CURRENT_ENV === "PRODUCTION" ||
-    process.env.NEXT_PUBLIC_USE_FIREBASE === "true"
+    process.env.CURRENT_ENV === 'PRODUCTION' ||
+    process.env.NEXT_PUBLIC_USE_FIREBASE === 'true'
   ) {
-    data = (await getAllDocuments("helps")).map((item) => ({
+    data = (await getAllDocuments('helps')).map((item) => ({
       docId: item.id,
       ...item.data,
     }));
   } else {
-    const jsonDirectory = path.join(process.cwd(), "helpsData");
-    const fileContents = await fs.readFile(jsonDirectory + "/helpsV2", "utf8");
+    const jsonDirectory = path.join(process.cwd(), 'helpsData');
+    const fileContents = await fs.readFile(jsonDirectory + '/helpsV2', 'utf8');
     data = JSON.parse(fileContents.toLocaleString()).map((item) => ({
       docId: item.id,
       ...item.data,
@@ -46,10 +47,10 @@ export default async function HelpsPage() {
       <Container maxWidth="xl">
         <div
           style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
             marginTop: 50,
           }}
         >
@@ -62,26 +63,26 @@ export default async function HelpsPage() {
           <Grid
             container
             style={{
-              display: "flex",
-              width: "100%",
-              justifyContent: "center",
+              display: 'flex',
+              width: '100%',
+              justifyContent: 'center',
               marginTop: 50,
             }}
           ></Grid>
           <Grid container>
             <Grid
               style={{
-                display: "flex",
-                flexDirection: "row",
+                display: 'flex',
+                flexDirection: 'row',
                 marginTop: 20,
-                alignItems: "center",
+                alignItems: 'center',
               }}
             ></Grid>
           </Grid>
           <Grid
             container
             style={{
-              justifyContent: "center",
+              justifyContent: 'center',
             }}
           >
             {helps?.map((help, ind) => (
