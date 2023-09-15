cae0b3a867a9b418f6bdf39c2b888adf5d3e6db6
Everkers <44750827+Everkers@users.noreply.github.com>
2023-09-15 15:49:41 +0100

    add prettier config and format all the files (#43) (#44)

--- src/utils/firebase/firestore/getDocument.js
+++ src/utils/firebase/firestore/getDocument.js
@@ -1,4 +1,10 @@
-import { getFirestore, doc, getDoc, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 import firebase_app from "../../../../config";
 
 const db = getFirestore(firebase_app);
