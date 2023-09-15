8f783f336f249c51ac82941e37f23ca1e35948ba
Everkers <waelkourich12@gmail.com>
2023-09-15 15:47:16 +0100

    add prettier config and format all the files

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json("Welcome to the most amazing API in the world!")
+  return NextResponse.json("Welcome to the most amazing API in the world!");
 }
