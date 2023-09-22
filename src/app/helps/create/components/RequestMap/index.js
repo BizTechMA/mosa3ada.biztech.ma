39f3e739345fccb61fab877aca608bca750d7eb3
Everkers <waelkourich12@gmail.com>
2023-09-22 04:36:23 +0100

    Add request creation flow

---
+++ src/app/helps/create/components/RequestMap/index.js
@@ -0,0 +1,60 @@
+"use client";
+import { Place } from "@mui/icons-material";
+import { forwardRef, useEffect, useMemo } from "react";
+import { useFormContext } from "react-hook-form";
+import Map, { Marker } from "react-map-gl";
+
+export const RequestMap = forwardRef(function InternalMap(
+  { manualMapChange, setManualMapChange },
+  ref,
+) {
+  const { setValue, watch } = useFormContext();
+
+  const mapValues = useMemo(
+    () => ({
+      longitude: watch("longitude") || -7.988836792391595,
+      latitude: watch("latitude") || 31.62309563018576,
+      zoom: watch("zoom") || 11.5,
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [watch("longitude"), watch("latitude")],
+  );
+
+  useEffect(() => {
+    if (manualMapChange) {
+      setManualMapChange(false);
+    }
+  }, [manualMapChange, setManualMapChange]);
+
+  return (
+    <Map
+      ref={ref}
+      mapLib={import("mapbox-gl")}
+      initialViewState={{
+        longitude: mapValues.longitude,
+        latitude: mapValues.latitude,
+        zoom: mapValues.zoom,
+      }}
+      onMove={(e) => {
+        setValue("longitude", e.viewState.longitude);
+        setValue("latitude", e.viewState.latitude);
+        setValue("zoom", e.viewState.zoom);
+      }}
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_BOX_ACCESS_TOKEN}
+      style={{ width: "100%", height: 400 }}
+      mapStyle="mapbox://styles/mapbox/outdoors-v12"
+    >
+      <Marker
+        longitude={mapValues.longitude}
+        latitude={mapValues.latitude}
+        anchor="bottom"
+      >
+        <Place
+          style={{
+            color: "red",
+          }}
+        />
+      </Marker>
+    </Map>
+  );
+});
