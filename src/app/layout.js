aed314b487914ad9f6b23f5d5ba0f18d5569c937
chaimae <56132945+s-shemmee@users.noreply.github.com>
2023-09-25 18:31:27 +0100

    Fix: Change favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import GoogleAnalytics from "@/utils/analytics";
 import { MuiProvider } from "@/components";
+import Head from 'next/head';
+
 export const metadata = {
   title: "مساعدة",
   description: "قائمة بالمناطق المتضررة المحتاجة للمساعدة",
@@ -9,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#faf1f5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#faf1f5" />
+      </Head>
       <MuiProvider>
         <GoogleAnalytics GA_TRACKING_ID={process.env.MEASUREMENT_ID} />
         <body suppressHydrationWarning>{children}</body>
@@ -16,3 +27,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
