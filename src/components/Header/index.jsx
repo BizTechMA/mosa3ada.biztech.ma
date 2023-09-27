a6cf1d0bd0679d8ddba159a7bed68292d81ad5a4
chaimae <56132945+s-shemmee@users.noreply.github.com>
2023-09-27 14:39:44 +0100

    Added the yellow info card to the page, addressing issue #70.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,39 @@
 "use client";
-import { Button, Container, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Container, Typography, IconButton } from "@mui/material";
+import ReportProblemRoundedIcon from '@mui/icons-material/ReportProblemRounded';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import { Close as CloseIcon } from "@mui/icons-material";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./header.module.css";
 
 const Header = ({ navComponent }) => {
+  const [showBanner, setShowBanner] = useState(true);
+
+  const handleDismissBanner = () => {
+    setShowBanner(false);
+  };
+
   return (
     <>
+      {showBanner && (
+        <div className={styles.banner}>
+          <Typography variant="body1" className={styles.bannerText} >
+            <ReportProblemRoundedIcon className={styles.bannerIcon} />
+            موقع مساعدة عبارة عن عمل تطوعي غير ربحي من طرف مجموعة  من الشباب المغاربة لا يمثل أي جهة أو مجموعة، هدفنا هو إيصال الإغاثة الإنسانية لضحايا زلزال المغرب.
+          </Typography>
+          <IconButton
+            aria-label="Dismiss"
+            color="#333"
+            size="small"
+            onClick={handleDismissBanner}
+          >
+            <CloseIcon />
+          </IconButton>
+        </div>
+      )}
+
       <header className={styles.container}>
         {/* Most likely the logo will be rendered here */}
         {navComponent && (
@@ -22,24 +49,28 @@ const Header = ({ navComponent }) => {
           </div>
         )}
 
-        <Container>
+        <Container maxWidth="xl">
           <div className={styles.headerBanner}>
             <div className={styles.contentContainer}>
               <Typography className={styles.typo}>
                 Mosa3ada.ma موقع للتضامن والمساعدة المجتمعية
               </Typography>
               <Link target="_blank" href={"https://t.co/wZ1Xh0CJLV"}>
-                <Button
-                  style={{
-                    fontSize: 18,
-                    marginTop: 30,
-                  }}
-                  size="large"
-                  variant="contained"
-                  color="primary"
-                >
-                  تواصل معنا
-                </Button>
+                <div className={styles.buttonContainer}>
+                  <Button
+                    style={{
+                      fontSize: 18,
+                      marginTop: 30,
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                  >
+                    <EmailOutlinedIcon style={{ marginLeft: '8px' }} /> تواصل معنا
+                  </Button>
+                </div>
               </Link>
             </div>
             <div className={styles.imgContainer}>
