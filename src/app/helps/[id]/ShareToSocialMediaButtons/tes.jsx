541e8e29fd683d590f0b1261503f0d5d3639c7aa
Itrkine Abdellah <48138349+Abdellah-Itrkine@users.noreply.github.com>
2023-10-19 00:07:46 +0100

    shard buttons with rea link

---
+++ src/app/helps/[id]/ShareToSocialMediaButtons/tes.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Dropdown from '@mui/joy/Dropdown';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+
+export default function BasicMenu() {
+    console.log('ss')
+  return (
+    <Dropdown>
+      <MenuButton>Dashboard...</MenuButton>
+      <Menu>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </Dropdown>
+  );
+}
