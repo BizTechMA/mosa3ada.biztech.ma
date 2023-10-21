28ea24851bea7a5060184bbee2e9aef37886a7d3
Itrkine Abdellah <48138349+Abdellah-Itrkine@users.noreply.github.com>
2023-10-21 19:47:04 +0100

    Social Media button drop menu working. I need to fix postio of the button in page jsx

--- src/app/helps/[id]/ShareToSocialMediaButtons/ShareButton.jsx
+++ src/app/helps/[id]/ShareToSocialMediaButtons/ShareButton.jsx
@@ -1,173 +1,11 @@
 'use client'
-// // import { Button, Typography } from '@mui/material';
-// // import  IosShareIcon from '@mui/icons-material/IosShare';
-// // // Define the button styles
-// // const buttonStyles = {
-// //   width: '88px',
-// //   height: '33px',
-// //   top: '12px',
-// //   left: '12px',
-// //   borderRadius: '4px',
-// //   padding: '6px 8px',
-// //   gap: '4px',
-// //   backgroundColor: '#F1F1F1', // Corrected color value
-// //   color: '#777777',
-// //   border: 'none',
-// //   cursor: 'pointer',
-// //   outline: 'none',
-// // };
-
-// // const iconStyles = {
-// //     width: '21px',
-// //     height: '21px',
-// //     padding: '3.5px 3.5px 2.5px 3.5px',
-// //   };
-
-// // export const ButtonShare = () => {
-// //   return (
-// //     <Button style={buttonStyles}>
-// //       <Typography style={{ color: '#777777' }}>مشاركة</Typography>
-// //       <IosShareIcon style={iconStyles} />
-// //     </Button>
-// //   );
-// // };
-
-
-// // import { Button, Typography, Menu, MenuItem } from '@mui/material';
-// // import IosShareIcon from '@mui/icons-material/IosShare';
-// // import React, { useState } from 'react';
-
-// // const buttonStyles = {
-// //   width: '88px',
-// //   height: '33px',
-// //   top: '12px',
-// //   left: '12px',
-// //   borderRadius: '4px',
-// //   padding: '6px 8px',
-// //   gap: '4px',
-// //   backgroundColor: '#F1F1F1',
-// //   color: '#777777',
-// //   border: 'none',
-// //   cursor: 'pointer',
-// //   outline: 'none',
-// // };
-
-// // const iconStyles = {
-// //   width: '21px',
-// //   height: '21px',
-// //   padding: '3.5px 3.5px 2.5px 3.5px',
-// // };
-
-// // export const ButtonShare = ({ shareUrl }) => {
-// //   const [anchorEl, setAnchorEl] = useState(null);
-
-// //   const handleMenuOpen = (event) => {
-// //     setAnchorEl(event.currentTarget);
-// //   };
-
-// //   const handleMenuClose = () => {
-// //     setAnchorEl(null);
-// //   };
-
-// //   const handleShareClick = (platform) => {
-// //     // Implement the logic to share the URL on the selected platform
-// //     // This is a placeholder, and you should replace it with your actual share functionality
-// //     alert(`Sharing on ${platform}: ${shareUrl}`);
-// //     handleMenuClose();
-// //   };
-
-// //   return (
-// //     <div>
-// //       <Button style={buttonStyles} onClick={handleMenuOpen}>
-// //         <Typography style={{ color: '#777777' }}>مشاركة</Typography>
-// //         <IosShareIcon style={iconStyles} />
-// //       </Button>
-// //       <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-// //         <MenuItem onClick={() => handleShareClick('Facebook')}>Share on Facebook</MenuItem>
-// //         <MenuItem onClick={() => handleShareClick('Twitter')}>Share on Twitter</MenuItem>
-// //         <MenuItem onClick={() => handleShareClick('WhatsApp')}>Share on WhatsApp</MenuItem>
-// //         <MenuItem onClick={handleMenuClose}>Cancel</MenuItem>
-// //       </Menu>
-// //     </div>
-// //   );
-// // };
-// import { Button, Typography, Menu, MenuItem } from '@mui/material';
-// import IosShareIcon from '@mui/icons-material/IosShare';
-// import React, { useState } from 'react';
-
-// const buttonStyles = {
-//   width: '88px',
-//   height: '33px',
-//   top: '12px',
-//   left: '12px',
-//   borderRadius: '4px',
-//   padding: '6px 8px',
-//   gap: '4px',
-//   backgroundColor: '#F1F1F1',
-//   color: '#777777',
-//   border: 'none',
-//   cursor: 'pointer',
-//   outline: 'none',
-// };
-
-// const iconStyles = {
-//   width: '21px',
-//   height: '21px',
-//   padding: '3.5px 3.5px 2.5px 3.5px',
-// };
-
-// export const ButtonShare = ({ shareUrl }) => {
-//   const [anchorEl, setAnchorEl] = useState(null);
-
-//   const handleMenuOpen = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleMenuClose = () => {
-//     setAnchorEl(null);
-//   };
-
-//   const handleShareClick = (platform, platformShareUrl) => {
-//     switch (platform) {
-//       case 'Facebook':
-//         platformShareUrl = 'https://www.example.com/share-on-facebook';
-//         break;
-//       case 'Twitter':
-//         platformShareUrl = 'https://www.example.com/share-on-twitter';
-//         break;
-//       case 'WhatsApp':
-//         platformShareUrl = 'https://www.example.com/share-on-whatsapp';
-//         break;
-//       default:
-//         platformShareUrl = '';
-//     }
-    
-//     // This is a placeholder, and you should replace it with your actual share functionality
-//     alert(`Sharing on ${platform}: ${platformShareUrl}`);
-//     handleMenuClose();
-//   };
-
-//   return (
-//     <>
-//       <Button style={buttonStyles} onClick={handleMenuOpen}>
-//         <Typography style={{ color: '#777777' }}>مشاركة</Typography>
-//         <IosShareIcon style={iconStyles} />
-//       </Button>
-//       <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-//         <MenuItem onClick={() => handleShareClick('Facebook', shareUrl)}>Share on Facebook</MenuItem>
-//         <MenuItem onClick={() => handleShareClick('Twitter', shareUrl)}>Share on Twitter</MenuItem>
-//         <MenuItem onClick={() => handleShareClick('WhatsApp', shareUrl)}>Share on WhatsApp</MenuItem>
-//         <MenuItem onClick={handleMenuClose}>Cancel</MenuItem>
-//       </Menu>
-//     </>
-//   );
-// };
-
 import React, { useState } from 'react';
 import { Button, Typography, Menu, MenuItem } from '@mui/material';
 import IosShareIcon from '@mui/icons-material/IosShare';
-
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn'; // Import Material-UI icons
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 const buttonStyles = {
     width: '88px',
     height: '33px',
@@ -188,17 +26,15 @@ const buttonStyles = {
     height: '21px',
     padding: '3.5px 3.5px 2.5px 3.5px',
   };
+
 export const ButtonShare = ({ shareUrl }) => {
   const [anchorEl, setAnchorEl] = useState(null);
-
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleMenuClose = () => {
     setAnchorEl(null);
   };
-
   const handleShareClick = (platform, shareUrl) => {
     switch (platform) {
         case 'Facebook':
@@ -219,16 +55,34 @@ export const ButtonShare = ({ shareUrl }) => {
   };
 
   return (
-    <>
+<>
       <Button style={buttonStyles} onClick={handleMenuOpen}>
         <Typography style={{ color: '#777777' }}>مشاركة</Typography>
-        <IosShareIcon style={iconStyles} />
       </Button>
       <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-        <MenuItem onClick={() => handleShareClick('Facebook', shareUrl)}>Share on Facebook</MenuItem>
-        <MenuItem onClick={() => handleShareClick('Twitter', shareUrl)}>Share on Twitter</MenuItem>
-        <MenuItem onClick={() => handleShareClick('WhatsApp', shareUrl)}>Share on WhatsApp</MenuItem>
-        <MenuItem onClick={() => handleShareClick('LinkedIn', shareUrl)}>WhatsApp
+        <MenuItem onClick={() => handleShareClick('Facebook', shareUrl)}>
+          <FacebookIcon color="primary" fontSize="small" /> 
+          <Typography>
+            شارك على فايسبوك
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={() => handleShareClick('Twitter', shareUrl)}>
+          <TwitterIcon color="primary" fontSize="small" /> 
+          <Typography>
+            شارك على تويتر
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={() => handleShareClick('WhatsApp', shareUrl)}>
+          <WhatsAppIcon color="primary" fontSize="small" />   
+          <Typography>
+            شارك على واتساب
+          </Typography>
+        </MenuItem>
+        <MenuItem onClick={() => handleShareClick('LinkedIn', shareUrl)}>
+          <LinkedInIcon color="primary" fontSize="small" />          
+           <Typography>
+            شارك على لينكدين
+          </Typography>
         </MenuItem>
       </Menu>
     </>
